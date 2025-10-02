4e66939845a8c60494a533be13022fc84a27a2b7
lmp2000 <lmrpais@gmail.com>
2025-10-02 21:24:16 +0100

    commit

---
+++ l01/rot_13.c
@@ -0,0 +1,24 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+    write (1, &c, 1);
+}
+
+int rot_13(char c)
+{
+    if ((c >= 'A' && c <= 'M') || (c >= 'a' && c >= 'm'))
+        c += 13;
+    else if ((c >= 'N' && c <= 'Z') || (c >= 'n' && c <= 'z'))
+        c -= 13;
+    return (c);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc == 2)
+        while (*argv[1])
+            ft_putchar(rot_13(*argv[1]++));
+    ft_putchar('\0');
+    return (0);
+}
