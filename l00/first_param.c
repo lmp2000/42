a1440aef88b4688516087e823ca3537d28f3c52e
lmp2000 <lmrpais@gmail.com>
2025-10-02 18:17:30 +0100

    commit

--- l00/first_param.c
+++ l00/first_param.c
@@ -3,22 +3,17 @@
 int main(int argc, char **argv)
 {
     char *ptr;
-
-    if (argc != 2)
+    
+    if (argc < 2)
     {
-        write (1, "a", 1);
         write (1, "\n", 1);
         return 0;
     }
 
-    ptr = argv[1];
+    ptr = *(argv + 1);
     while (*ptr)
     {
-        if (*ptr == 'a')
-        {
-            write (1, ptr, 1);
-            break;
-        }
+        write (1, ptr, 1);
         ptr++;
     }
     write (1, "\n", 1);
